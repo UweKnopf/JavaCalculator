feec48c70578a2d0c314e06005b2a96a42d7ce13
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2024-04-29 13:18:54 +0200

    Added View Doc and scaffolding methods

--- CalcView.java
+++ CalcView.java
@@ -1,3 +1,15 @@
+import javax.swing.*;
+
 public class CalcView {
+
+    // First Time initialization: window with buttons and display(a label)
+    public CalcView() {
+
+    }
+
+    // Updating only the display based on the controller
+    public void UpdateCalcView() {
+
+    }
 
 }
