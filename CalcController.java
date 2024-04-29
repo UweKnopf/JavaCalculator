ed058861299fa6e1ba580107d23b1173412f35b0
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2024-04-29 20:55:47 +0200

    Basic controller (no body)

--- CalcController.java
+++ CalcController.java
@@ -1,3 +1,10 @@
 public class CalcController {
+    private CalcView view;
+    private CalcModel model;
+
+    public CalcController(CalcView view, CalcModel model) {
+        this.view = view;
+        this.model = model;
+    }
 
 }
