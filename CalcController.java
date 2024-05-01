5020518cf78f5a887bfd5cb5dfeb98e2999452c3
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2024-05-01 23:18:18 +0200

    Basic OpAction scaffolding

--- CalcController.java
+++ CalcController.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 
 public class CalcController {
     private CalcView view;
@@ -15,13 +16,49 @@ public class CalcController {
             final int iCopy = i;
             view.ButtonNumArray[i].addActionListener(e -> numButtonAction(iCopy));
         }
+
+        for (int i = 0; i < view.ButtonOpArray.length; i++) {
+            final int iCopy = i;
+            view.ButtonOpArray[i].addActionListener(e -> numOpAction(iCopy));
+        }
     }
 
     private void numButtonAction(int num) {
         if (model.getOperator() == CalcModel.Operator.NIL) {
             model.setFirstNum(num + model.getFirstNum() * 10);
         }
-        // System.err.println(Integer.toString(model.getFirstNum()));
+        System.err.println(Integer.toString(model.getFirstNum()));
     }
 
+    private void numOpAction(int num) {
+        switch (num) {
+            // mult x
+            case 0:
+
+                break;
+            // dev /
+            case 1:
+
+                break;
+            // add +
+            case 2:
+
+                break;
+            // sub -
+            case 3:
+
+                break;
+            // equal =
+            case 4:
+
+                break;
+            // Clear
+            case 5:
+
+                break;
+            default:
+                throw new NoSuchElementException("This Button should not exist");
+
+        }
+    }
 }
