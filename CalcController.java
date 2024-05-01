f45cdac723640dfb6c012fb056a138d0304a7784
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2024-05-01 19:26:23 +0200

    NumButton presses are getting registered

--- CalcController.java
+++ CalcController.java
@@ -1,3 +1,4 @@
+
 public class CalcController {
     private CalcView view;
     private CalcModel model;
@@ -5,6 +6,22 @@ public class CalcController {
     public CalcController(CalcView view, CalcModel model) {
         this.view = view;
         this.model = model;
+
+    }
+
+    // Gets the Buttons from View and adds all the corresponding methods to them
+    public void actionBinding() {
+        for (int i = 0; i < view.ButtonNumArray.length; i++) {
+            final int iCopy = i;
+            view.ButtonNumArray[i].addActionListener(e -> numButtonAction(iCopy));
+        }
+    }
+
+    private void numButtonAction(int num) {
+        if (model.getOperator() == CalcModel.Operator.NIL) {
+            model.setFirstNum(num + model.getFirstNum() * 10);
+        }
+        // System.err.println(Integer.toString(model.getFirstNum()));
     }
 
 }
