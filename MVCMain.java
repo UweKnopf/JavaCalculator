d16a35813d3201664dc9bdc237eaea794bfc2ce3
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2024-05-01 23:18:37 +0200

    WIP Main func for testing

--- MVCMain.java
+++ MVCMain.java
@@ -1,6 +1,10 @@
 public class MVCMain {
 
     public static void main(String[] args) {
+        CalcView view = new CalcView();
+        CalcModel model = new CalcModel();
+        CalcController controller = new CalcController(view, model);
+        controller.actionBinding();
 
     }
 }
