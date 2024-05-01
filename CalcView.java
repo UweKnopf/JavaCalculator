ec5a61424bcdcf1ea84d42938ee10a2e854780d8
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2024-05-01 14:49:26 +0200

    Added first basic UI conforming to all requirements

--- CalcView.java
+++ CalcView.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
 
 /*
  * Buttons 0-9 in one Array and Grid (Handled in CalcCon)
@@ -9,23 +11,40 @@ import java.awt.GridLayout;
 public class CalcView {
 
     public JButton[] ButtonNumArray = new JButton[10];
+    public JButton[] ButtonOpArray = { new JButton("x"), new JButton("/"), new JButton("+"), new JButton("-"),
+            new JButton("=") };
+    public JLabel Display;
 
     // First Time initialization: window with buttons and display
     public CalcView() {
         JFrame f = new JFrame();// creating instance of JFrame
-        GridLayout ButtonNumLayout = new GridLayout(4, 3);
+        Display = new JLabel("0", SwingConstants.RIGHT);
+        JPanel NumPanel = new JPanel(new GridLayout(4, 3)); // container for the number keys
+        JPanel OpPanel = new JPanel(new GridLayout(3, 2));
 
+        f.add(Display);
+
+        // TODO: MAKE THE CLASSIC NUMPAD ORDER
         // Create and and 10 Buttons for the numkeys
         for (int i = 9; i >= 0; i--) {
             // Creating Buttons in the array where Index corresponds to value
             ButtonNumArray[i] = new JButton(Integer.toString(i));
 
-            // adding button in JFrame
-            f.add(ButtonNumArray[i]);
+            // adding button to the Panel
+            NumPanel.add(ButtonNumArray[i]);
+        }
+        f.add(NumPanel);
+
+        // Adding all JButtons to the Panel
+        for (int i = 0; i < 5; i++) {
+            // adding button to the Panel
+            OpPanel.add(ButtonOpArray[i]);
         }
 
-        f.setSize(400, 500);// 400 width and 500 height for the window
-        f.setLayout(ButtonNumLayout);// using a grid layout
+        f.add(OpPanel);
+
+        f.setSize(500, 500);// 400 width and 500 height for the window
+        f.setLayout(new GridLayout(3, 1));// using a grid layout
         f.setVisible(true);// making the frame visible
 
     }
