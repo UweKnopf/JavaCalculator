eb3052f154fa4768a6cef401fb24246e7637e946
UweKnopf <69891951+UweKnopf@users.noreply.github.com>
2024-05-04 23:52:37 +0200

    More or less working (in console only)

--- CalcController.java
+++ CalcController.java
@@ -26,39 +26,95 @@ public class CalcController {
     private void numButtonAction(int num) {
         if (model.getOperator() == CalcModel.Operator.NIL) {
             model.setFirstNum(num + model.getFirstNum() * 10);
+        } else {
+            model.setSecondNum(num + model.getSecondNum() * 10);
         }
         System.err.println(Integer.toString(model.getFirstNum()));
+        System.err.println(Integer.toString(model.getSecondNum()));
     }
 
     private void numOpAction(int num) {
         switch (num) {
             // mult x
             case 0:
+                model.setOperator(CalcModel.Operator.MULTI);
 
                 break;
             // dev /
             case 1:
+                model.setOperator(CalcModel.Operator.DEV);
 
                 break;
             // add +
             case 2:
+                model.setOperator(CalcModel.Operator.ADD);
 
                 break;
             // sub -
             case 3:
+                model.setOperator(CalcModel.Operator.SUB);
 
                 break;
             // equal =
             case 4:
+                // new method to calculate?
+                // model.setOperator(CalcModel.Operator.CAL);
+                if (model.getOperator() == CalcModel.Operator.NIL) {
+
+                } else {
+                    calculate();
+                }
+
+                // model.setFirstNum(num + model.getFirstNum() * 10);
 
                 break;
             // Clear
             case 5:
+                // Maybe better to set this somewhere else by using the setOperator
+                model.setFirstNum(0);
+                model.setSecondNum(0);
+                model.setOperator(CalcModel.Operator.NIL);
 
                 break;
             default:
                 throw new NoSuchElementException("This Button should not exist");
 
         }
+    }
+
+    private void calculate() {
+        switch (model.getOperator()) {
+            case MULTI:
+                model.setFirstNum(model.getFirstNum() * model.getSecondNum());
+                model.setSecondNum(0);
+                break;
+
+            case DEV:
+                model.setFirstNum(model.getFirstNum() / model.getSecondNum());
+                model.setSecondNum(0);
+
+                break;
+
+            case ADD:
+                model.setFirstNum(model.getFirstNum() + model.getSecondNum());
+                model.setSecondNum(0);
+
+                break;
+
+            case SUB:
+                model.setFirstNum(model.getFirstNum() - model.getSecondNum());
+                model.setSecondNum(0);
+
+                break;
+
+            default:
+                break;
+        }
+
+        model.setOperator(CalcModel.Operator.NIL);
+
+        System.err.println(Integer.toString(model.getFirstNum()));
+        System.err.println(Integer.toString(model.getSecondNum()));
+
     }
 }
